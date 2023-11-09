139ba4ceef43a0975928c8ecc17232e39cc86572
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2023-11-08 21:20:33 -0300

    First Model-Controller-View - Department.

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /*public void AddSeller(Seller seller)
+        {
+
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+        }*/
+    }
+}
