c6dd09e30e37268f5d363b7515316e997f672c71
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2023-11-16 11:15:46 -0300

    Preparing sales search navigation views.

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+	public class SalesRecordsController : Controller
+	{
+		// GET: SalesRecords
+		public async Task<IActionResult> Index()
+		{
+			return View();
+		}
+
+		// GET: SimpleSearch
+		public async Task<IActionResult> SimpleSearch()
+		{
+			return View();
+		}
+
+		// GET: GroupingSearch
+		public async Task<IActionResult> GroupingSearch()
+		{
+			return View();
+		}
+	}
+}
